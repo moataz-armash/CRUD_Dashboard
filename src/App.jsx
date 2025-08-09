5803fb42fd1d159f51522548b31b0f22857aba69
MahmoudRafaat20 <Mahmoudrafaatmahmoud3@gmail.com>
2025-08-09 15:10:35 +0300

    login-register

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Card } from 'flowbite-react'
+
+import Register from './Components/Register/Register'
+import Login from './Components/Login/Login'
 
 function App() {
   
 
   return (
     <>
-   
+     <Register/>
+     <Login/>
     </>
   )
 }
